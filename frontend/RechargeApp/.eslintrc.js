ac10a935d5316b1bd4996984b4526e78af7dc74a
bbq0638-design <bbq0638@gmail.com>
2025-12-18 15:10:53 +0900

    env삭제

--- frontend/RechargeApp/.eslintrc.js
+++ frontend/RechargeApp/.eslintrc.js
@@ -1,14 +1,10 @@
 module.exports = {
   root: true,
-  extends: [
-    '@react-native-community',
-    'airbnb',
-    'prettier'
-  ],
+  extends: ['@react-native-community', 'airbnb', 'prettier'],
   rules: {
     'import/prefer-default-export': 'off',
     'import/extensions': ['off'],
     // React Native에서 .js 파일에도 JSX 문법 사용 허용 (필요시 사용)
-    'react/jsx-filename-extension': ['error', { extensions: ['.js', '.jsx'] }],
+    'react/jsx-filename-extension': ['error', {extensions: ['.js', '.jsx']}],
   },
 };
